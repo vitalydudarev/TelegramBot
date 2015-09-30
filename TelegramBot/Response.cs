9a79c6b6d10d435760db878d3c1222ab90ddaa17
Vitaly Dudarev <vitalydudarev@gmail.com>
2015-09-30 23:31:53 +0300

    parse json response using JsonParser.

--- TelegramBot/Response.cs
+++ TelegramBot/Response.cs
@@ -3,12 +3,40 @@ using System.Net;
 
 namespace TelegramBot
 {
-    public class Response
+    public class Response<T> where T : class
     {
-        public string GetResponse(HttpWebRequest request)
+        public bool HasErrors { get { return _error != null; } }
+        public Error Error { get { return _error; } }
+        public T Result { get {return GetResult(); } }
+
+        private string _responseString;
+        private Error _error;
+        private JsonParser _parser;
+
+        public Response(string responseString)
         {
-            var response = (HttpWebResponse)request.GetResponse();
-            return new StreamReader(response.GetResponseStream()).ReadToEnd();
+            _responseString = responseString;
+            _parser = new JsonParser();
         }
+            
+        private T GetResult()
+        {
+            var token = _parser.GetToken(_responseString, "ok");
+
+            if (!bool.Parse(token))
+            {
+                _error = _parser.Parse<Error> (_responseString);
+
+                return (T)null;
+            }
+            
+            return _parser.Parse<T>(_responseString);
+        }
+    }
+
+    public class Error
+    {
+        public int ErrorCode { get; set; }
+        public string Description { get; set; }
     }
 }
