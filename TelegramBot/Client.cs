fae20d5dc96b4e3d07bd5c52eed2ceb0c76ce4d4
Vitaly Dudarev <vitalydudarev@gmail.com>
2015-10-12 23:38:47 +0300

    added Client class which executes web requests.

---
+++ TelegramBot/Client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace TelegramBot
+{
+    public interface IClient
+    {
+        string Send(Request request);
+    }
+
+    public class Client : IClient
+    {
+        private readonly string _uri;
+
+        public Client(string uri)
+        {
+            _uri = uri;
+        }
+
+        public string Send(Request request)
+        {
+            Stream responseStream;
+
+            try
+            {
+                var response = (HttpWebResponse)request.CreateRequest(_uri).GetResponse();
+                responseStream = response.GetResponseStream();
+            }
+            catch (WebException e)
+            {
+                responseStream = e.Response.GetResponseStream();
+            }
+
+            return responseStream == null ? null : new StreamReader(responseStream).ReadToEnd();
+        }
+    }
+}
+
