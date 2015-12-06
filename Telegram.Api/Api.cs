43250cc9db821f16a90ae208b9561d8b8596b4b4
Vitaly Dudarev <vitalydudarev@gmail.com>
2015-12-06 14:28:37 +0300

    extended GetUpdates() method.

--- Telegram.Api/Api.cs
+++ Telegram.Api/Api.cs
@@ -1,22 +1,34 @@
 ﻿using System.Collections.Generic;
 
-namespace TelegramBot
+namespace Telegram.Api
 {
     public class Api
     {
         private string _accessToken;
-        private string _uri;
+        private readonly string _uri;
+        private readonly ResponseParser _responseParser;
 
         public Api(string accessToken)
         {
             _accessToken = accessToken;
             _uri = @"https://api.telegram.org/bot" + _accessToken + "/";
+            _responseParser = new ResponseParser();
         }
 
-        public string GetUpdates()
+        public Update[] GetUpdates(int offset = 0, int limit = 100, int timeout = 0)
         {
+            var parameters = new Dictionary<string, string>
+            {
+                { "offset", offset.ToString() },
+                { "limit", limit.ToString() },
+                { "timeout", timeout.ToString() }
+            };
+
             var client = new Client(_uri + "getUpdates");
-            return client.Send(new GetRequest());
+            var request = new GetRequest(parameters);
+            var response = client.Send(request);
+
+            return _responseParser.Parse<Update[]>(response);
         }
 
         public string SendMessage(int userId, string message)
