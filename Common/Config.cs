ac8dd4de5cd06f6b8b892dbe44668123a9e753e1
Vitaly Dudarev <vitalydudarev@gmail.com>
2016-12-27 13:32:53 +0300

    Added Bot/Api config.

---
+++ Common/Config.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Common
+{
+    public class Config
+    {
+        public string AccessToken { get; set; }
+        public int? BotOwnerId { get; set; }
+        public string LogsDirectory { get; set; }
+
+        public void Validate()
+        {
+            if (string.IsNullOrEmpty(AccessToken))
+                throw new Exception("Access token is not provided.");
+        }
+
+        public static Config Get(string fileName)
+        {
+            var text = File.ReadAllText(fileName);
+
+            return JsonConvert.DeserializeObject<Config>(text);
+        }
+    }
+}
--- Telegram.Bot/Program.cs
+++ Telegram.Bot/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
+using Common;
 
 namespace Telegram.Bot
 {
@@ -13,7 +10,10 @@ namespace Telegram.Bot
 
         static void Main(string[] args)
         {
-            _bot = new Bot();
+            var config = Config.Get("config.json");
+            config.Validate();
+
+            _bot = new Bot(config.AccessToken);
 
             var thread = new Thread(Run);
             thread.Start();
